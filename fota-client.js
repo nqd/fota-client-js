1d6c989be56c1a47c5c391edb0896d460968619e
nqdinh <nqdinhddt@gmail.com>
2015-06-21 16:42:05 +0700

    handle error

--- fota-client.js
+++ fota-client.js
@@ -10,12 +10,7 @@ var HOST = "192.168.1.163"
 var PROTOCOL = "http"
 var APIKEY = "db396724456fa1ece579b134e00c05d0d851431aff0decad4852e2bf48a8ad64"
 
-/* parsing input */
-console.log(argv.version)
-console.log(argv.application)
-console.log(argv.i1)
-console.log(argv.i2)
-
+/* checking input */
 if (!(argv.version && argv.i1 && argv.i2 && argv.application)) {
   console.error('Need to provide version, image 1 path, and image 2 path');
   return;
@@ -25,7 +20,6 @@ var files = {
   image1: argv.i1,
   image2: argv.i2
 }
-
 
 /* upload firmwares */
 var uploaded_url = [];
@@ -53,6 +47,9 @@ async.forEachOf(files, function(value, key, callback) {
     if (err) {
       return callback(err);
     }
+    if (httpResponse.statusCode != 201) {
+      return callback(new Error('Cannot upload firmware'))
+    }
     body = JSON.parse(body);
     uploaded_url.push({
       name: key,
@@ -62,12 +59,12 @@ async.forEachOf(files, function(value, key, callback) {
         pathname: body.parseUrl.path
       })
     })
-    // console.log(body)
     callback();
   });
 }, function(err) {
-  if (err) 
+  if (err) {
     return console.error(err.message);
+  }
 
   console.log('Uploaded images successful', uploaded_url);
   register_version(uploaded_url);
@@ -90,18 +87,17 @@ function register_version(firmware_urls) {
     }
   }
 
-  // console.log(firmware_urls);
   register_version_options.body = JSON.stringify({
     "version": argv.version,
     "firmwares": firmware_urls
   });
   request.post(register_version_options, function optionalCallback(err, httpResponse, body) {
     if (err) {
-      return console.error('upload firmware data failed:', err);
+      return console.error('Register failed:', err);
     }
-    if (body.error) {
-      return console.error("Failed to register new version ", body.error)
-    };
+    if (httpResponse.statusCode != 201) {
+      return console.error("Failed to register new version, make sure you did create new application!")
+    }
     console.log('Register application ' + argv.application + ', version ' + argv.version +' successful!');
   });
 }
